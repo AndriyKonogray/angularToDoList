8e5d98c658728fd32187f3b7615f65d7fe6bcd13
andriikonoh <andriikon@tahometer.com>
2019-03-19 18:16:20 +0200

    generate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import {} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +7,8 @@ import {} from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDoList';
-  selectedTask: any;
-  newTaskName: any;
-  tasks = [
-    {name: 'read book'},
-    {name: 'read doc'},
-    {name: 'practice'}
-  ];
 
-  deleteTask(task) {
-    this.tasks = this.tasks.filter(t => {
-      return t.name != this.selectedTask;
-    });
-  }
-
-  selectTask(task) {
-    this.selectedTask = task;
-  }
-
-  isSelected(task) {
-    return this.selectedTask === task;
-  }
-
-  addTask() {
-    this.tasks.push({ name: this.newTaskName});
-    this.newTaskName = '';
-  }
-  comleteTask() {
-  }
+@Input() task;
+@Output() select = new EventEmitter();
 }
 
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-task',
@@ -10,6 +10,33 @@ export class TaskComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectedTask: any;
+  newTaskName: any;
+  tasks = [
+    {name: 'read book'},
+    {name: 'read doc'},
+    {name: 'practice'}
+  ];
+
+  deleteTask(task) {
+    this.tasks = this.tasks.filter(t => {
+      return t.name != this.selectedTask;
+    });
+  }
+
+  selectTask(task) {
+    this.selectedTask = task;
+  }
+
+  isSelected(task) {
+    return this.selectedTask === task;
+  }
+
+  addTask() {
+    this.tasks.push({ name: this.newTaskName});
+    this.newTaskName = '';
   }
 
 }
