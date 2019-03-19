087603797f415524cff0fc42c2d5a36697e52932
andriikonoh <andriikon@tahometer.com>
2019-03-19 17:55:11 +0200

    generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectComponent } from './projects/project/project.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { InputFormComponent } from './input-form/input-form.component';
+import { TaskComponent } from './tasks/task/task.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProjectsComponent,
+    ProjectComponent,
+    TasksComponent,
+    InputFormComponent,
+    TaskComponent,
   ],
   imports: [
     BrowserModule,
